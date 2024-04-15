e8a15229f83c87988a06f3774ff1b616cad8b474
daidi06 <37501896+daidi06@users.noreply.github.com>
2024-04-15 01:20:05 +0000

    shorten nqme

--- main.R
+++ main.R
@@ -3,10 +3,10 @@ library(here)
 head(gss_cat)
 dim(gss_cat)
 
-tv_hours_table <-  gss_cat %>% 
+tv_hours_tab <-  gss_cat %>% 
   filter(age < 30) %>% 
   group_by(marital) %>% 
   summarise(mean_tv_hours = mean(tvhours, na.rm = TRUE)) %>% 
   arrange(mean_tv_hours) 
 
-write_csv(tv_hours_table, path = here("TV_hours_by_Marital.csv"))
+write_csv(tv_hours_tab, path = here("TV_hours_by_Marital.csv"))
