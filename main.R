78e9bc84882656678c6b18f79b62388e842fd9d7
daidi06 <ildidier@hotmail.com>
2024-04-15 01:22:54 +0000

    make names longer

--- main.R
+++ main.R
@@ -3,10 +3,10 @@ library(here)
 head(gss_cat)
 dim(gss_cat)
 
-tv_hours_table <-  gss_cat %>% 
+tv_hours_tabulation <-  gss_cat %>% 
   filter(age < 30) %>% 
   group_by(marital) %>% 
   summarise(mean_tv_hours = mean(tvhours, na.rm = TRUE)) %>% 
   arrange(mean_tv_hours) 
 
-write_csv(tv_hours_table, path = here("TV_hours_by_Marital.csv"))
+write_csv(tv_hours_tabulation, path = here("TV_hours_by_Marital.csv"))
